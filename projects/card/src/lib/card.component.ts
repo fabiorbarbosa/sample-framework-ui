59d229ff556377807e7f83a6ba63b278f5abfc35
Fábio Barbosa <fabiobarbosaa@gmail.com>
2025-07-03 12:20:42 -0300

    fix: set schemas CUSTOM_ELEMENTS_SCHEMA

--- projects/card/src/lib/card.component.ts
+++ projects/card/src/lib/card.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'lib-card',
   standalone: true,
   imports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   template: `
     <p>
       card works!
--- projects/input/src/lib/input.component.ts
+++ projects/input/src/lib/input.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'lib-input',
   standalone: true,
   imports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   template: `
     <p>
       input works!
